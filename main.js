9d7f10eed271727b9a3ca726c9679dbd6442e294
zakwan93 <zakwan_bhaiyat@yahoo.com>
2018-12-13 17:06:08 -0500

    make an axios call for one pokemon and retrive a data pf that pokemon

--- main.js
+++ main.js
@@ -17,3 +17,45 @@ Squi.addEventListener('click',function(){
 	Squi.classList.remove("Squirtle");
 	Squi.classList.add("Squir")
 })
+
+var card1 = document.getElementById("mycard")
+
+var bulbasaur_pokemon = axios.get("http://fizal.me/pokeapi/api/v2/name/bulbasaur.json")
+.then(function (myResponse) {
+	console.log(myResponse);
+	var data = myResponse.data;
+	var name = data.name;
+	var height = data.height;
+	var hp = data.stats[5].base_stat
+	var attack = data.stats[1].base_stat
+	var defence = data.stats[3].base_stat
+	var abilities = data.abilities
+	var myabilities = [];
+	for (var i=0;i<abilities.length;i++){
+		myabilities.push(abilities[i].ability.name)
+	}
+	var moves = data.moves
+	// console.log(moves)
+	var myMoves = [];
+	for (var i=0;i<moves.length;i++){
+			var topmoves = moves[i].move.name	
+			if(topmoves.length <5){
+				myMoves.push(topmoves)
+			}
+	}
+
+	console.log(myMoves)
+	// console.log(myMoves)
+	// console.log(myabilities)
+	// console.log(height);
+
+	card1.innerHTML = 
+	`<h3 class = "pl-5 ml-4">NAME - ${name}
+	<h5 class = "pl-5 ml-4">HEIGHT - ${height}</h5>
+	<h5 class = "pl-5 ml-4">HP - ${hp}</h5>
+	<h5 class = "pl-5 ml-4">ATTACK - ${attack}</h5>
+	<h5 class = "pl-5 ml-4">DEFENCE - ${defence}</h5>
+	<h5 class = "pl-5 ml-4">TOP 5 MOVES - ${myMoves}</h5>
+	<h5 class = "pl-5 ml-4">ABILITIES - ${myabilities}</h5>`
+
+})
